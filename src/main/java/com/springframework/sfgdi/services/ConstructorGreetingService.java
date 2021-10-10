7e5f0c2a73293e7645e9fda97403c6147a62649b
Leigh West <leighwest@hotmail.com>
2021-10-10 16:48:43 +1100

    dependency injection basics

---
+++ src/main/java/com/springframework/sfgdi/services/ConstructorGreetingService.java
@@ -0,0 +1,14 @@
+package com.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Service;
+
+
+
+
+@Service
+public class ConstructorGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World! - Constructor";
+    }
+}
---
+++ src/test/java/com/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package com.springframework.sfgdi.controllers;
+
+import com.springframework.sfgdi.services.ConstructorGreetingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        // second least preferred: using setter to inject the greeting service implementation
+        controller.setGreetingService(new ConstructorGreetingService());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
